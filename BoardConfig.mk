2c61f38eab2669a69305faf5b751664b604d9a81
ripee <ripee.dev@gmail.com>
2020-07-16 14:57:00 +0200

    zeroltexx: Define BT name in common tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,9 +26,6 @@
 # Assert
 TARGET_OTA_ASSERT_DEVICE := zerolte,zeroltexx,zerolteskt
 
-# Bluetooth
-BOARD_BLUETOOTH_OID_BUILDCFG_INCLUDE_DIR := device/samsung/zeroltexx/bluetooth
-
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zeroltexx/include
 
